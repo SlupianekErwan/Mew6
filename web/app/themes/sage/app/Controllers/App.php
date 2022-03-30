0bb176aec910487c234021a77168b7b73dfa83ef
erwan <bmaxpro@gmail.com>
2022-03-30 13:37:43 +0200

    init ripo

--- web/app/themes/sage/app/Controllers/App.php
+++ web/app/themes/sage/app/Controllers/App.php
@@ -35,4 +35,15 @@ class App extends Controller
     {
         return get_template_directory_uri() . '/assets/images/logo.svg';
     }
+
+    // public function getLanguageOptions()
+    // {
+    //     if( !function_exists('pll_the_languages')) return;
+
+    //     return array(
+    //         'language_menu' => pll_the_languages( array('raw' => 1)),
+    //         'current_language' => pll_current_languages()
+    //     );
+
+    // }
 }
